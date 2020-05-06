db25b821fa5b6cf75e899936cae55a4d8d2c149e
robbymcm <robert.mcmahon@otterbein.edu>
2020-05-06 19:09:04 -0400

    added create repo functionality, added paths.json, driver links, errors

--- repo.py
+++ repo.py
@@ -1,14 +1,78 @@
 from selenium import webdriver
 import json
+import sys
 
-driver = webdriver.Chrome('/Users/robert/Desktop/current/CreateRepo/chromedriver')
 
+## Retrieve data from paths.json
+def get_paths():
+    with open('paths.json', "r") as f:
+        data = json.load(f)
+        chrome_path = data['chromedriver']
+        repo_path = data['repo_path']
+    return (chrome_path, repo_path)
+
+
+# set paths, create driver
+paths = get_paths()
+driver = webdriver.Chrome(paths[0])
+repo_path = paths[1]
+
+
+# Dictionary of necessary github links
+driver_links = {
+    'url': 'https://github.com/login',
+    'username': 'login_field',                                          # by id
+    'password': 'password',                                             # by id
+    'sign_in_btn': 'commit',                                            # by name
+    'new_btn': '/html/body/div[4]/div/aside[1]/div[2]/div[2]/div/h2/a', # by xpath onwards
+    'repo_name': '/html/body/div[4]/main/div/form/div[2]/auto-check/dl/dd/input',
+    'public': '/html/body/div[4]/main/div/form/div[3]/div[1]/label/input',
+    'private': '/html/body/div[4]/main/div/form/div[3]/div[2]/label/input',
+    'readme': '/html/body/div[4]/main/div/form/div[3]/div[4]/div[1]/label/input[2]',
+    'create_btn': '/html/body/div[4]/main/div/form/div[3]/button'
+}
+
+# Errors
+errors = {
+    "E_REPO_NAME": 'Usage: repo.py <repository_name>',
+    "E_PERMISSIONS": 'Please choose either: `-p` for private or `-g` for global'
+}
+
+
+## Retrieve login data from `login.json`
 def get_login_data():
     with open('login.json', "r") as f:
         data = json.load(f)
         user_name = data['username']
         password = data['password']
+    if user_name == "" or password == "":
+        print("Please enter your username and password")
+        exit()
     return (user_name, password)
+
+
+## process command line arguments
+def get_repo_name():
+    name = ""
+    permission = "-p"
+    permissions = ["-p", "-g"]
+
+    # repo name
+    if len(sys.argv) == 1:
+        print(errors['E_REPO_NAME'])
+        exit()
+    else:
+        name = sys.argv[1]
+    
+    # permissions
+    if len(sys.argv) == 3:
+        if sys.argv[2] not in permissions:
+            print(errors['E_PERMISSIONS'])
+            exit()
+        elif sys.argv[2] == permissions[1]:
+            permission = "-g"
+
+    return (name, permission)
 
 
 ## Login to github.com
@@ -20,4 +84,19 @@ def login():
     driver.find_element_by_id('password').send_keys(login_info[1])
     driver.find_element_by_name('commit').click()
 
-login()
+
+## Create repo
+def create_repo(name, permission):
+    driver.find_element_by_xpath(driver_links['new_btn']).click()
+    driver.find_element_by_xpath(driver_links['repo_name']).send_keys(name)
+    if permission == '-p':
+        driver.find_element_by_xpath(driver_links['private']).click()
+
+    driver.find_element_by_xpath(driver_links['readme']).click()
+    driver.find_element_by_xpath(driver_links['create_btn']).click()
+
+
+if __name__ == "__main__":
+    repository = get_repo_name()
+    login()
+    create_repo(repository[0], repository[1])
